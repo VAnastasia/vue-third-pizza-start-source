69faca990ab32711e70c76542b6016e687d77bfc
Anastasiya <v.anastasiya26@mail.ru>
2024-02-11 18:11:33 +0700

    module 3

---
+++ frontend/src/router/routes.js
@@ -0,0 +1,45 @@
+export const routes = [
+  {
+    path: "",
+    name: "home",
+    component: () => import("@/views/HomeView.vue"),
+    meta: { layout: "DefaultLayout" },
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/LoginView.vue"),
+    meta: { layout: "SimpleLayout" },
+  },
+  {
+    path: "/cart",
+    name: "cart",
+    component: () => import("@/views/CartView.vue"),
+    meta: { layout: "DefaultLayout" },
+  },
+  {
+    path: "/user",
+    name: "user",
+    meta: { layout: "DefaultLayout" },
+    component: () => import("@/views/UserView.vue"),
+    /*
+     * В шаблонах использована одна и та же страница,
+     * на которой меняется лишь часть контента
+     * в зависимости от выбранной вкладки.
+     *
+     * Поэтому здесь разумно использовать дочерние маршруты.
+     */
+    children: [
+      {
+        path: "orders",
+        name: "orders",
+        component: () => import("@/views/OrdersView.vue"),
+      },
+      {
+        path: "profile",
+        name: "profile",
+        component: () => import("@/views/ProfileView.vue"),
+      },
+    ],
+  },
+];
